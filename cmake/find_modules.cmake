147f6f7b6babeb7a024e08e4771d7811f2ad6c72
Jamie-Rhys Edwards <jamie_airfix@yahoo.co.uk>
2018-08-29 13:46:06 +0100

    Initial Commit

--- cmake/find_modules.cmake
+++ cmake/find_modules.cmake
@@ -2,7 +2,6 @@ set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_SOURCE_DIR}/cmake/find_module
 
 set(GTEST_ROOT "${CMAKE_SOURCE_DIR}/ThirdParty/gtest/1.7.0/" CACHE PATH "Path to GTest")
 set(EASYLOGGINGPP_ROOT "${CMAKE_SOURCE_DIR}/ThirdParty/easyloggingpp/9.96.4/" CACHE PATH "Path to EasyLogging++")
-set(BOOST_ROOT )
 
 find_package(gtest REQUIRED)
 find_package(EasyLOGGINGPP REQUIRED)
